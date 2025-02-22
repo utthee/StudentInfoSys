3e6a9a1a3e4df295f1f05295cfdc3fe868f87b88
utthee <yusufalimacalangan@gmail.com>
2025-02-22 22:18:47 +0800

    Add files via upload

--- main_drive.py
+++ main_drive.py
@@ -1,11 +1,15 @@
 import sys
 import csv
 import re
-import os
+#import os
 
-from collections import defaultdict
+#from collections import defaultdict
 from mainUI import Ui_MainWindow
 from update import Ui_Dialog
+from updateProgram import Ui_ProgramDialog
+from updateCollege  import Ui_CollegeDialog
+
+
 from PyQt5.QtWidgets import *
 from PyQt5 import *
 
@@ -23,6 +27,10 @@ class MainClass(QMainWindow, Ui_MainWindow):
         self.current_table_index = self.displayComboBox.currentIndex()  # Track the current table being displayed
         self.data = [[], [], []]  # Store CSV data for each table
         self.open_csv_file()
+
+        self.readStudentCSV()
+        self.readProgramCSV()
+        self.readCollegeCSV()
         
         self.addStudentButton.clicked.connect(self.addStudentEntry)
         self.addProgramButton.clicked.connect(self.addProgramEntry)
@@ -33,7 +41,8 @@ class MainClass(QMainWindow, Ui_MainWindow):
 
         self.sortComboBox.currentIndexChanged.connect(self.sortLayout)
         self.searchButton.clicked.connect(self.searchEntry)
-        self.displayComboBox.currentIndexChanged.connect(self.switch_table)
+
+        self.displayComboBox.currentIndexChanged.connect(self.displayTable)
 
     def open_csv_file(self):
         for i, file_path in enumerate(self.file_paths):
@@ -48,30 +57,87 @@ class MainClass(QMainWindow, Ui_MainWindow):
         self.programChoices()
         self.collegeChoices()
         
-        self.display_table(self.displayComboBox.currentIndex())
+        self.displayTable()
 
-    def display_table(self, table_index):
-        self.current_table_index = table_index
-        self.tableWidget.setColumnCount(len(self.headers[table_index]))
-        self.tableWidget.setHorizontalHeaderLabels(self.headers[table_index])
-        self.tableWidget.setRowCount(len(self.data[table_index]))
+    def displayTable(self):
+        for row in range(self.tableWidget.rowCount()):
+            self.tableWidget.setRowHidden(row, False)  # Reset hidden rows
+
+        if self.displayComboBox.currentIndex() == 0:
+            self.displayStudentTable()
+            return
+
+        elif self.displayComboBox.currentIndex() == 1:
+            self.displayProgramTable()
+            return
+
+        elif self.displayComboBox.currentIndex() == 2:
+            self.displayCollegeTable()
+            return
+    
+    def displayStudentTable(self):
+        with open(STUDENT_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            self.headers[0] = next(reader)  # Keep headers
+            data = [row for row in reader]
+            self.data[0] = data
+
+        self.tableWidget.setColumnCount(len(self.headers[0]))
+        self.tableWidget.setHorizontalHeaderLabels(self.headers[0])
+        self.tableWidget.setRowCount(len(self.data[0]))
         
-        for row_idx, row in enumerate(self.data[table_index]):
+        for row_idx, row in enumerate(self.data[0]):
             for col_idx, value in enumerate(row):
                 self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(str(value)))
         
         self.sortComboBox.clear()
-        self.sortComboBox.addItems(self.headers[self.current_table_index])
-
+        self.sortComboBox.addItems(self.headers[0])
+        
         self.searchComboBox.clear()
-        self.searchComboBox.addItems(self.headers[self.current_table_index])
+        self.searchComboBox.addItems(self.headers[0])
+    
+    def displayProgramTable(self):
+        self.sortComboBox.clear()
+        self.sortComboBox.addItems(self.headers[1])
+        
+        self.searchBox.clear()
+        self.searchComboBox.clear()
+        self.searchComboBox.addItems(self.headers[1])
+        
+        with open(PROGRAM_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            self.headers[1] = next(reader)  # Keep headers
+            data = [row for row in reader]
+            self.data[1] = data
 
-    def switch_table(self):
-        new_index = self.displayComboBox.currentIndex()
-        self.display_table(new_index)
+        self.tableWidget.setColumnCount(len(self.headers[1]))
+        self.tableWidget.setHorizontalHeaderLabels(self.headers[1])
+        self.tableWidget.setRowCount(len(self.data[1]))
+        
+        for row_idx, row in enumerate(self.data[1]):
+            for col_idx, value in enumerate(row):
+                self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(str(value)))
 
-        #self.sortComboBox.clear()
-        #self.sortComboBox.addItems(self.headers)
+    def displayCollegeTable(self):
+        with open(COLLEGE_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            self.headers[2] = next(reader)  # Keep headers
+            data = [row for row in reader]
+            self.data[2] = data
+
+        self.tableWidget.setColumnCount(len(self.headers[2]))
+        self.tableWidget.setHorizontalHeaderLabels(self.headers[2])
+        self.tableWidget.setRowCount(len(self.data[2]))
+        
+        for row_idx, row in enumerate(self.data[2]):
+            for col_idx, value in enumerate(row):
+                self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(str(value)))
+        
+        self.sortComboBox.clear()
+        self.sortComboBox.addItems(self.headers[1])
+        
+        self.searchComboBox.clear()
+        self.searchComboBox.addItems(self.headers[1])
     
     def searchEntry(self):
         searched_item = self.searchBox.text().lower()
@@ -86,11 +152,13 @@ class MainClass(QMainWindow, Ui_MainWindow):
             cell_text = cell_item.text().lower() if cell_item else ""
             self.tableWidget.setRowHidden(row, searched_item not in cell_text)
 
+
         """
         for row in range(self.tableWidget.rowCount()):
             match = any(searched_item in (self.tableWidget.item(row, col).text().lower() if self.tableWidget.item(row, col) else "") for col in range(self.tableWidget.columnCount()))
             self.tableWidget.setRowHidden(row, not match)
         """
+        
 
     def deleteEntry(self):
         if self.displayComboBox.currentIndex() == 0:
@@ -143,7 +211,62 @@ class MainClass(QMainWindow, Ui_MainWindow):
         self.updateStudentCSV(data)
         self.open_csv_file()
 
-##################################################################################  STUDENT   #############################################################################################
+    def updateProgramEntry(self):
+        self.readProgramCSV()
+        selected_row = self.tableWidget.currentRow()
+
+        data = self.data[1]
+
+        if 0 <= selected_row < len(data):
+            updateProgramCode = self.tableWidget.item(selected_row, 0).text()
+            updateProgramName = self.tableWidget.item(selected_row, 1).text()
+            updateCollegeCode = self.tableWidget.item(selected_row, 2).text()
+
+            editor = Dialog(updateProgramCode, updateProgramName, updateCollegeCode)
+            if editor.exec_():
+                updated_values = editor.updatedProgramData()
+                
+                if 0 <= selected_row < len(data):
+                    del data[selected_row]
+
+                for row in self.data[0]:
+                    if row[5] == updateProgramCode:
+                        row[5] = updated_values[0]
+
+                self.data[1].append(updated_values)
+                self.updateProgramCSV(self.data[1])
+                self.updateStudentCSV(self.data[0])
+                self.open_csv_file()
+
+    def updateCollegeEntry(self):
+        self.readCollegeCSV()
+        selected_row = self.tableWidget.currentRow()
+
+        data = self.data[2]
+
+        if 0 <= selected_row < len(data):
+            updateCollegeCode = self.tableWidget.item(selected_row, 0).text()
+            updateCollegeName = self.tableWidget.item(selected_row, 1).text()
+
+            editor = Dialog(updateCollegeCode, updateCollegeName)
+            if editor.exec_():
+                updated_values = editor.updatedCollegeData()
+                
+                if 0 <= selected_row < len(data):
+                    del data[selected_row]
+
+                for row in self.data[1]:
+                    if row[2] == updateCollegeCode:
+                        row[2] = updated_values[0]
+
+                self.data[2].append(updated_values)
+                self.updateCollegeCSV(self.data[2])
+                self.updateProgramCSV(self.data[1])
+                self.open_csv_file()
+
+#--------------------------------------------------------------------------------     STUDENT     ----------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------     STUDENT     ----------------------------------------------------------------------
 
     def addStudentEntry(self):
         self.readStudentCSV()
@@ -280,14 +403,16 @@ class MainClass(QMainWindow, Ui_MainWindow):
                 for col_idx, value in enumerate(row):
                     self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(value))
     
+#--------------------------------------------------------------------------------     PROGRAM     ----------------------------------------------------------------------
 
-################################################################################################    PROGRAM     ###############################################################################
+#--------------------------------------------------------------------------------     PROGRAM     ----------------------------------------------------------------------
+
 
     def addProgramEntry(self):
         self.readProgramCSV()
         
         programCode = self.programCodeEdit2.text().strip().replace(" ","").upper()
-        programName = self.programNameEdit.text().strip()
+        programName = self.programNameEdit.text().strip().title()
         collegeCode = self.collegeCodeBox.currentText()
 
         if self.current_table_index != 1:
@@ -370,7 +495,9 @@ class MainClass(QMainWindow, Ui_MainWindow):
             self.programCodeBox.addItem("")
             self.programCodeBox.addItems(program_codes)
 
-########################################################################################  COLLEGE  ##############################################################################
+#--------------------------------------------------------------------------------     COLLEGE     ----------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------     COLLEGE     ----------------------------------------------------------------------
 
     def addCollegeEntry(self):
         self.readCollegeCSV()
@@ -466,9 +593,9 @@ class MainClass(QMainWindow, Ui_MainWindow):
             self.collegeCodeBox.addItem("")
             self.collegeCodeBox.addItems(college_codes)
 
-#----------------------------------------------------------------------- EDIT PROGRAM ----------------------------------------------------------------------
+#----------------------------------------------------------------------- EDIT STUDENT ----------------------------------------------------------------------
 
-#----------------------------------------------------------------------- EDIT PROGRAM ------------------------------------------------------------------
+#----------------------------------------------------------------------- EDIT STUDENT ------------------------------------------------------------------
 
 class Dialog(QDialog, Ui_Dialog):
     def __init__(self, idNumber, firstName, lastName, yearLevel, gender, programCode):
@@ -519,8 +646,127 @@ class Dialog(QDialog, Ui_Dialog):
             self.updateGenderComboBox.currentText(),
             self.updateProgramCodeComboBox.currentText(),
         ]
-            
+#----------------------------------------------------------------------- EDIT PROGRAM ----------------------------------------------------------------------
 
+#----------------------------------------------------------------------- EDIT PROGRAM ------------------------------------------------------------------
+             
+class Dialog(QDialog, Ui_ProgramDialog):
+    def __init__(self, programCode, programName, collegeCode):
+        super().__init__()
+        self.setupUi(self)
+        self.collegeChoices()
+
+        # Input current data on to the line edits.
+        self.programCodeEdit.setText(programCode)
+        self.programNameEdit.setText(programName)
+        self.collegeCodeBox.setCurrentText(collegeCode)
+
+        self.confirmButton.clicked.connect(self.accept)       
+        self.cancelButton.clicked.connect(self.reject)
+
+    def collegeChoices(self):
+        with open(COLLEGE_CSV, "r", newline='') as file:
+            reader = csv.reader(file)
+            next(reader, None)
+            college_codes = sorted(set(row[0] for row in reader if len(row) > 0 and row[0].strip()))
+
+            self.collegeCodeBox.clear()
+            self.collegeCodeBox.addItems(college_codes)
+
+    def readProgramCSV(self):
+        with open(PROGRAM_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader, None)
+            self.data = [row for row in reader]
+
+    def updatedProgramData(self):
+        self.readProgramCSV()
+
+        if not (self.programCodeEdit and self.programNameEdit and self.collegeCodeBox):
+            QMessageBox.warning(self, "Input Error", "All fields must be filled up.")
+            return
+        
+        if not all(char.isalpha() or char.isspace() for char in self.programCodeEdit.text().replace(" ","").upper() and self.programNameEdit.text().strip().title()):
+            QMessageBox.warning(self, "Input Error", "Please input a valid program name.")
+            return
+        
+        if any(program[0] == self.programCodeEdit.text().strip().replace(" ","").upper() for program in self.data):
+            QMessageBox.warning(self, "Input Error", "The program you are trying to add already exists.")
+            return
+        
+        # Checks whether there are already existing inputs or not.
+        with open(PROGRAM_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader, None)
+
+            programNameDupe = self.programNameEdit.text().strip().replace(" ","").upper()
+
+            for row in reader:
+                if row[1].strip().replace(" ","").upper() == programNameDupe:
+                    QMessageBox.warning(self, "Input Error", "The program you are trying to enter already exists.")
+                    return
+
+        return [
+            self.programCodeEdit.text().strip().replace(" ","").upper(),
+            self.programNameEdit.text().strip().title(),
+            self.collegeCodeBox.currentText()
+        ]
+    
+#----------------------------------------------------------------------- EDIT COLLEGE ----------------------------------------------------------------------
+
+#----------------------------------------------------------------------- EDIT COLLEGE ------------------------------------------------------------------
+
+class Dialog(QDialog, Ui_CollegeDialog):
+    def __init__(self, collegeCode, collegeName):
+        super().__init__()
+        self.setupUi(self)
+
+        # Input current data on to the line edits.
+        self.collegeCodeEdit.setText(collegeCode)
+        self.collegeNameEdit.setText(collegeName)
+
+
+        if self.confirmButton.clicked.connect(self.verifyCollegeData()):
+
+            self.confirmButton.clicked.connect(self.accept)       
+        self.cancelButton.clicked.connect(self.reject)
+
+    def readCollegeCSV(self):
+        with open(COLLEGE_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader, None)
+            self.data = [row for row in reader]
+
+    def updatedCollegeData(self):
+        self.readCollegeCSV()
+
+        if not self.collegeCodeEdit or not self.collegeNameEdit:
+            QMessageBox.warning(self, "Input Error", "All fields must be filled up.")
+            return
+        
+        if not all(char.isalpha() or char.isspace() for char in self.collegeCodeEdit.text().strip().replace(" ","").upper() and self.collegeNameEdit.text().strip().title()):
+            QMessageBox.warning(self, "Input Error", "Please input a valid college name.")
+            return
+        
+        if any(college[0] == self.collegeCodeEdit.text().strip().replace(" ","").upper() for college in self.data):
+            QMessageBox.warning(self, "Input Error", "The college you are trying to enter already exists.")
+            return
+        
+        with open(COLLEGE_CSV, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader, None)
+
+            collegeNameDupe = self.collegeNameEdit.text().strip().replace(" ","").upper()
+
+            for row in reader:
+                if row[1].strip().replace(" ","").upper() == collegeNameDupe:
+                    QMessageBox.warning(self, "Input Error", "The college you are trying to enter already exists.")
+                    return
+
+        return [
+            self.collegeCodeEdit.text().strip().replace(" ","").upper(),
+            self.collegeNameEdit.text().strip().title()
+        ]
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
